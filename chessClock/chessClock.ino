601dee8cbbb44f3c98fbb83f728415038b553d35
joshua.junker <joshiklis@gmail.com>
2024-05-17 00:36:20 -0300

    Alterar codigo para suportar LCD I2C

--- chessClock/chessClock.ino
+++ chessClock/chessClock.ino
@@ -1,17 +1,11 @@
-#include <LiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
 
 // Initial configuration
 #define BUTTON_PIN 2
 #define BUTTON_START_STOP 3
 
-
 // LCD pins
-#define rs 12
-#define en 11
-#define d4 4 
-#define d5 5
-#define d6 6
-#define d7 7
+#define LCD_ADDRESS 0x27
 
 // LCD cursor coordinates
 int cursorT0Active = 0;
@@ -24,25 +18,25 @@ int t1TimePOints = 12;
 int t0TimeMs = 13;
 int cursorIncrementS = 2;
 int cursorIncrementPoints = 6;
-int  cursorIncrementMs = 7;
+int cursorIncrementMs = 7;
 
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7); // Init LCD handler
+LiquidCrystal_I2C lcd(LCD_ADDRESS, 16, 2); // Init LCD handler
 
-long actualTime_0, actualTime_1, timeReference; // Variables to store each player remaining time counter and last time reading in ms
-long *activeTimePointer = &actualTime_0; // Pointer to the active player time pointer, initialized with player 0 counter
-int activeIncrement; // Int with the time increment in ms
+long actualTime_0, actualTime_1, timeReference;        // Variables to store each player remaining time counter and last time reading in ms
+long *activeTimePointer = &actualTime_0;               // Pointer to the active player time pointer, initialized with player 0 counter
+int activeIncrement;                                   // Int with the time increment in ms
 bool playRoutineFlag = false, previousPlayRoutineFlag; // Boolean flags to control play/plause and transitions
-bool changeActiveTimeFlag = false; // Boolean flag to indicate that the player counter must be switched
-bool activeTime = false; // boolean to see wich player counter is active: player0 = false , player1 = true
+bool changeActiveTimeFlag = false;                     // Boolean flag to indicate that the player counter must be switched
+bool activeTime = false;                               // boolean to see wich player counter is active: player0 = false , player1 = true
 
+void setupInitialClockConfig(long initialTime_0 = 300000, long initialTime_1 = 300000, long increment = 3000);
 
-void setupInitialClockConfig(long initialTime_0=300000, long initialTime_1=300000, long increment=3000);
-
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
 
-  //Initialize Serial Communiction
-  Serial.begin(9600);lcd.clear();
+  // Initialize Serial Communiction
+  Serial.begin(9600);
 
   // Initialize lcd
   initLCD();
@@ -58,23 +52,28 @@ void setup() {
   setupInitialClockConfig(); // Initialize the clock with default config - Variables can be passed here!
 }
 
-void loop() {
-  
-  if (playRoutineFlag) {
+void loop()
+{
+
+  if (playRoutineFlag)
+  {
     // if we are in play routine
-    if (!previousPlayRoutineFlag) {
+    if (!previousPlayRoutineFlag)
+    {
       // and previously we where not, enter on play routine
       enterPlayRoutine();
     }
 
-   
     subtractTimeDelta(); // if it's not the first play routine cycle, subtract the elapsed time from time active time counter
-    checkForLoss(); // verify if the game was lost by time for the active time counter
+    checkForLoss();      // verify if the game was lost by time for the active time counter
 
     printStatistics(); // print the cycle status
-  } else {
+  }
+  else
+  {
     // if we are not on play routine, hence, paused
-    if (previousPlayRoutineFlag) {
+    if (previousPlayRoutineFlag)
+    {
       // and whe where before
       Serial.println("Paused...");
       previousPlayRoutineFlag = playRoutineFlag; // And our previous play routine starts to be false
@@ -82,28 +81,34 @@ void loop() {
   }
 
   // independetly of the active play routine, if changeActiveTime Flag is on, call the changeTimeRoutine
-  if (changeActiveTimeFlag) {
-      changeActiveTime();
-    }
+  if (changeActiveTimeFlag)
+  {
+    changeActiveTime();
+  }
 }
 
-
-void initLCD() {
+void initLCD()
+{
   // Simple initial print function
-  lcd.begin(16, 2);
+  lcd.init();
+  lcd.backlight();
+
+  // Prints a initial message
   lcd.setCursor(0, 0);
   lcd.print("GoJu Chess Clock");
   delay(1500);
-  //lcd.clear();
+  lcd.clear();
 }
 
-void enterPlayRoutine (){
+void enterPlayRoutine()
+{
   // Enter the play routine, starting the time reference and putting the previous play routine as the actual
   timeReference = millis();
   previousPlayRoutineFlag = playRoutineFlag;
 }
 
-void setupInitialClockConfig (long initialTime_0=300000, long initialTime_1=300000, long increment=3000){
+void setupInitialClockConfig(long initialTime_0 = 300000, long initialTime_1 = 300000, long increment = 3000)
+{
   // Starts both players time counters and increments with the default or provided times
   actualTime_0 = initialTime_0;
   actualTime_1 = initialTime_1;
@@ -114,14 +119,19 @@ void setupInitialClockConfig (long initialTime_0=300000, long initialTime_1=3000
   return;
 }
 
-void checkForLoss() {
+void checkForLoss()
+{
   // Verify if the active time counter have runned out of time
-  if (*activeTimePointer <= 0) {
+  if (*activeTimePointer <= 0)
+  {
     lcd.clear();
     // iF active time pointer value is equal or minor than zero, verify wich player lost and prints it
-    if (activeTime) {
+    if (activeTime)
+    {
       lcd.print(" WINNER | LOSER");
-    } else {
+    }
+    else
+    {
       lcd.print(" LOSER | WINNER");
     }
     // Put the time as 0 to not display a negative value and stops the game
@@ -130,30 +140,36 @@ void checkForLoss() {
   }
 }
 
-void subtractTimeDelta() {
+void subtractTimeDelta()
+{
   // Calculate the time between now and the last measure
   long newMeasure = millis();
   long timeDelta = newMeasure - timeReference;
 
-  //Set the new time reading reference as now
+  // Set the new time reading reference as now
   timeReference = newMeasure;
 
   // And subtracts the time delta from the active counter
   *activeTimePointer -= timeDelta;
 }
 
-void changeActiveTime() {
+void changeActiveTime()
+{
   // Takes the active time pointer, add the increment to its time and change the active pointer and it's flags
 
-  if (playRoutineFlag) {
+  if (playRoutineFlag)
+  {
     // If in play routine, add increment, to avoid incrementing time when paused and not started
     *activeTimePointer += activeIncrement;
   }
 
   // set the new time counter based on wich counter is active: if 0 is active, set the new as 1 and vice-versa
-  if (activeTime) {
+  if (activeTime)
+  {
     activeTimePointer = &actualTime_0;
-  } else {
+  }
+  else
+  {
     activeTimePointer = &actualTime_1;
   }
 
@@ -162,37 +178,44 @@ void changeActiveTime() {
   changeActiveTimeFlag = false;
 }
 
-void printStatistics(){
+void printStatistics()
+{
   // Prints statistics in a single line on Serial Monitor
   float actualTime_0_seconds = actualTime_0 / 1000.0; // Convert to seconds
   float actualTime_1_seconds = actualTime_1 / 1000.0; // Convert to seconds
   lcd.setCursor(0, 1);
   conversiontime(actualTime_0_seconds);
-  lcd.print("/"); conversiontime(actualTime_1_seconds);
+  lcd.print("/");
+  conversiontime(actualTime_1_seconds);
 }
 
-void startStopInterrupt() {
+void startStopInterrupt()
+{
   // Switch the value of the play routine
   playRoutineFlag = !playRoutineFlag;
 }
 
-void changeActiveTimeInterrupt() {
+void changeActiveTimeInterrupt()
+{
   // Indicates tha the active time counter must be switched
   changeActiveTimeFlag = true;
 }
 
-void conversiontime(long timeInS){
+void conversiontime(long timeInS)
+{
   int hours = timeInS / 3600;
   int minutes = timeInS % 3600 / 60;
   int seconds = timeInS % 3600 % 60;
   lcd.print(hours);
   lcd.print(":");
-  if(minutes < 10){
+  if (minutes < 10)
+  {
     lcd.print("0");
   }
   lcd.print(minutes);
   lcd.print(":");
-  if(seconds < 10){
+  if (seconds < 10)
+  {
     lcd.print("0");
   }
   lcd.print(seconds);
